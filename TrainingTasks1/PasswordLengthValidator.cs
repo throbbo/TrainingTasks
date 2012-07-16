d4b7fc5e8d70a1fe97c4dbc8765c602c9177a6f2
Rob Sumsion <rob@gothere.com.au>
2012-07-16 15:37:00 +1000

    Commit for proj files

---
+++ TrainingTasks1/PasswordLengthValidator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TrainingTasks1
+{
+    public class PasswordLengthValidator : IPasswordValidator
+    {
+        public PasswordLengthValidator()
+        {
+            _message = "Password Length must be greater than 9."; 
+        }
+
+        private readonly string  _message;
+        public bool IsValid
+        {
+            get { return  (_message ?? "").Length > 9; }
+        }
+
+        public string Message
+        {
+            get { return _message; }
+        }
+    }
+}
