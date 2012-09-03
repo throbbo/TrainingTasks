c503e820c0bda143c0aff5bf0d8ede9f115f0ccc
Rob Sumsion <rob@gothere.com.au>
2012-09-03 16:38:11 +1000

    Implemented PassAlternateListToListAndReturnNewListWithAlternatingValuesFromEachList Test and Class and Method

--- TrainingTasks5/NewLinqOperatorTests.cs
+++ TrainingTasks5/NewLinqOperatorTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using NUnit.Framework;
+using Robs.LinqExtensions;
 
 namespace TrainingTasks5
 {
@@ -16,9 +17,34 @@ namespace TrainingTasks5
         // eg. list.Alternate(list2);
 
         [Test]
-        public void Test1()
+        public void WhereIfLinqTakesBooleanPredicateAndOnlyEvaluatesThePredicateIfTheConditionIsTrue()
         {
             
+        }
+
+        [Test]
+        public void PassAlternateListToListAndReturnNewListWithAlternatingValuesFromEachList()
+        {
+            var list1 = Data.GetProducts().Where(x => x.ProductID <= 5);
+            var list2 = Data.GetProducts().Where(x => x.ProductID >= 6).Take(5);
+
+            var listExpected = new List<Product>();
+            for (int i = 1; i <= 5; i++)
+            {
+                listExpected.Add(Data.GetProducts().Single(x => x.ProductID==i));
+                listExpected.Add(Data.GetProducts().Single(x => x.ProductID==i+5));
+            }
+
+            var listActual = list1.Alternate(list2).ToList();
+            for (int i = 0; i < listExpected.Count; i++)
+            {
+                Assert.AreEqual(listExpected[i].Category, listActual[i].Category);
+                Assert.AreEqual(listExpected[i].ProductID, listActual[i].ProductID);
+                Assert.AreEqual(listExpected[i].UnitPrice, listActual[i].UnitPrice);
+                Assert.AreEqual(listExpected[i].ProductName, listActual[i].ProductName);
+                Assert.AreEqual(listExpected[i].UnitsInStock, listActual[i].UnitsInStock);
+            }
+
         }
     }
 }
