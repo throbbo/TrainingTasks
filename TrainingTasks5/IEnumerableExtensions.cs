99a0b8dcc7bc3219057ba8fb900c4adb1ab58360
Rob Sumsion <rob@gothere.com.au>
2012-09-04 11:05:56 +1000

    AHA

--- TrainingTasks5/IEnumerableExtensions.cs
+++ TrainingTasks5/IEnumerableExtensions.cs
@@ -22,19 +22,31 @@ namespace Robs.LinqExtensions
             return listReturn;
         }
 
-        public static IEnumerable<T> Alternate<T> (this IEnumerable<T> list, IEnumerable<T> listAlt)
+        public static IEnumerable<T> Alternate<T>(this IEnumerable<T> list, IEnumerable<T> listAlt)
         {
-            var listReturn = new List<T>();
-            var list1 = list.ToList();
-            var list2 = listAlt.ToList();
-            if(list1.Count > list2.Count) throw new Exception("Lists are of unequal Lengths");
+            using (IEnumerator<T> e1 = list.GetEnumerator())
+            using (IEnumerator<T> e2 = listAlt.GetEnumerator())
+                while (e1.MoveNext() && e2.MoveNext())
+                {
+                    yield return e1.Current;
+                    yield return e2.Current;
+                }
 
-            for (int i = 0; i < list1.Count; i++)
-            {
-                listReturn.Add(list1[i]);
-                listReturn.Add(list2[i]);
-            }
-            return listReturn.AsEnumerable();
         }
+
+        //public static IEnumerable<T> Alternate<T> (this IEnumerable<T> list, IEnumerable<T> listAlt)
+        //{
+        //    var listReturn = new List<T>();
+        //    var list1 = list.ToList();
+        //    var list2 = listAlt.ToList();
+        //    if(list1.Count > list2.Count) throw new Exception("Lists are of unequal Lengths");
+
+        //    for (int i = 0; i < list1.Count; i++)
+        //    {
+        //        listReturn.Add(list1[i]);
+        //        listReturn.Add(list2[i]);
+        //    }
+        //    return listReturn.AsEnumerable();
+        //}
     }
 }
